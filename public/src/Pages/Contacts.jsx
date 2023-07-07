4206233797444ec618c6fe2f419b70984c6b37af
raku <lakjinteo@gmail.com>
2023-07-08 01:08:09 +0800

    2:29:56

--- public/src/Pages/Contacts.jsx
+++ public/src/Pages/Contacts.jsx
@@ -59,80 +59,87 @@ export default function Contacts({ contacts, currentUser }) {
 }
 
 const Container = styled.div`
-    display:grid;
-    grid-template-rows:10% 75% 15%;
+    display: grid;
+    grid-template-rows: 10% 75% 15%;
     overflow: hidden;
-    background-color:#080420;
+    background-color: #080420;
     .brand {
-        display:flex;
-        align-items:center;
-        justify-content:center;
-        gap:1rem;
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+        justify-content: center;
         img {
             height:2rem;
         }
         h3 {
-            color:white;
-            text-transform:uppercase;
+            color: white;
+            text-transform: uppercase;
         }
     }
     .contacts {
-        display:flex;
-        flex-direction:column;
-        align-items:cetner;
-        overflow:auto;
-        gap:0.8rem;
-        .contact: {
-            background-color: #ffffff39;
-            min-height:5rem;
-            width:90%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        overflow: auto;
+        gap: 0.8rem;
+        &::-webkit-scrollbar {
+            width: 0.2rem;
+            &-thumb {
+                background-color: #ffffff39;
+                width: 0.1rem;
+                border-radius: 1rem;
+            }
+        }
+        .contact {
+            background-color: #ffffff34;
+            min-height: 5rem;
+            width: 90%;
             cursor:pointer;
-            border-radius:0.2rem;
-            padding:0.4rem;
-            gap:1rem;
-            align-items:center;
-            display:flex;
-            transition: 0.5ms ease-in-out;
+            border-radius: 0.2rem;
+            padding: 0.4rem;
+            gap: 1rem;
+            display: flex;
+            align-items: center;
+            transition: 0.5s ease-in-out;
             .avatar {
                 img {
-                    height:3rem;
+                  height: 3rem;
                 }
             }
             .username {
                 h3 {
-                    color:white;
+                    color: white;
                 }
             }
         }
         .selected {
-            background-color:#9186f3; 
+            background-color: #9a86f3; 
         }
     }
     .current-user {
-        background-color:#0d0d30;
-        display:flex;
-        justify-content:center;
-        align-items:center;
-        gap:2rem;
+        background-color: #0d0d30;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 2rem;
         .avatar {
             img {
-                height:4rem;
-                max-inline-size:100%;
+                height: 4rem;
+                max-inline-size: 100%;
             }
         }
         .username {
             h2 {
-                color:white;
+                color: white;
             }
         }
-        @media screen and (min-width:720px) and (max-width:1080px) {
-            gap:0.5rem;
+        @media screen and (min-width: 720px) and (max-width: 1080px) {
+            gap: 0.5rem;
             .username {
                 h2 {
-                    font-size:1rem;
+                    font-size: 1rem;
                 }
             }
         }
     }
-    
 `
